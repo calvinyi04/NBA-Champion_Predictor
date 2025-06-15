3e83e884c3347829e27ce21c5eeaf09126d3fc63
Calvin Yi <157909819+calvinyi04@users.noreply.github.com>
2025-06-15 09:06:39 -0500

    Add files via upload

--- main.py
+++ main.py
@@ -5,8 +5,6 @@ from sklearn.ensemble import RandomForestClassifier
 from sklearn.model_selection import train_test_split, cross_val_score, GridSearchCV
 from sklearn.preprocessing import StandardScaler
 from sklearn.metrics import classification_report, confusion_matrix, roc_auc_score, roc_curve
-import matplotlib.pyplot as plt
-import seaborn as sns
 
 # Constants
 DB_PATH = 'nba.sqlite'
